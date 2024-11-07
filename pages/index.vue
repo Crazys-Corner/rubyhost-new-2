0afeaf6d601487ce58cdfcdf0b5a16fbc32be751
crazys_corner <craazygaming1021@gmail.com>
2024-11-06 20:14:16 -0500

    patch

--- pages/index.vue
+++ pages/index.vue
@@ -1,52 +1,54 @@
 <script setup>
-import Lenis from '@studio-freight/lenis';
 import { onMounted } from 'vue';
+import Lenis from '@studio-freight/lenis';
 import gsap from 'gsap';
 
-function isMobileDevice() {
-  return /Mobi|Android/i.test(navigator.userAgent);
-}
-if (!isMobileDevice()) {
 onMounted(() => {
-  const lenis = new Lenis({
-    smooth: true,
-    direction: 'vertical',
-    gestureDirection: 'vertical',
-    smoothTouch: true,
-    touchMultiplier: 2,
-  });
-
-  lenis.on('scroll', (e) => {
-    // Custom GSAP animations can be triggered here
-    gsap.to('.animate-class', {
-      y: -100,
-      opacity: 1,
-      duration: 1,
-      scrollTrigger: {
-        trigger: '.animate-class',
-        start: 'top center',
-        end: 'bottom top',
-        scrub: true,
-      },
-    });
-  });
-
-  function raf(time) {
-    lenis.raf(time);
-    requestAnimationFrame(raf);
+  // This code runs only on the client side
+  function isMobileDevice() {
+    return /Mobi|Android/i.test(navigator.userAgent);
   }
 
-  requestAnimationFrame(raf);
+  if (!isMobileDevice()) {
+    const lenis = new Lenis({
+      smooth: true,
+      direction: 'vertical',
+      gestureDirection: 'vertical',
+      smoothTouch: true,
+      touchMultiplier: 2,
+    });
+
+    lenis.on('scroll', (e) => {
+      // Custom GSAP animations can be triggered here
+      gsap.to('.animate-class', {
+        y: -100,
+        opacity: 1,
+        duration: 1,
+        scrollTrigger: {
+          trigger: '.animate-class',
+          start: 'top center',
+          end: 'bottom top',
+          scrub: true,
+        },
+      });
+    });
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+  } else {
+    console.log('Lenis is disabled on mobile devices');
+  }
 });
-}
 </script>
-
-
 <template>
   <div data-scroll-container>
     <Navbar />
 
-    <div >
+    <div>
       <HeroSection />
     </div>
 
