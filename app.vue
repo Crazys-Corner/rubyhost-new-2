3ffe9742b680900b3142165fc9c2b39fcb995c41
crazys-corner <craazygaming1021@gmail.com>
2024-11-08 14:34:19 -0500

    WIP Bar

--- app.vue
+++ app.vue
@@ -1,4 +1,8 @@
 <template>
+  <div class="bg-red-600 text-white text-center py-4">
+  This site is a work in progress, please disregard any dead links, bugs or other issues. Thank you for your understanding.
+</div>
+
 <NuxtPage />
 </template>
 <style>
